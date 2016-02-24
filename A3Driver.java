55d0f98b8c8dcb86c0257cfef38d47255e4f76ba
jaibockLEE <jaibock@hotmail.com>
2016-02-23 18:48:26 -0600

    Cleaned up some comments, style

--- A3Driver.java
+++ A3Driver.java
@@ -6,46 +6,38 @@ import java.util.Iterator;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class A3Driver 
-{
+public class A3Driver {
   private static ArrayList<Item> shoppingCart = new ArrayList<Item>(); // shopping cart list
 	
   public static void main(String[] args){
 	  try{  
-		  //Open file; file name specified in args (command line)
 		  String filename = args[0]; // name of file
 		  FileReader reader = new FileReader(filename);
 		  BufferedReader fhand = new BufferedReader(reader);
 		  
-		  //Parse input transactions
-		  // input ex: <operation> <category> <name> <price> <quantity> <weight>
-		  //           <optional field1> <optional field2>
-		  String line; // transaction string
+		  String line; 
 		  while((line = fhand.readLine()) != null){
-			  // harvest operation and data
 			  line = line.trim();
 			  String operation, data;
 			  int npos = line.indexOf(' ');
 			  
-			  if(npos > 0){ //multiple parameters
+			  if(npos > 0){ 
 				  operation = line.substring(0, npos).toLowerCase();
 				  data = line.substring(npos+1);
 			  }
-			  else{ //single parameter
+			  else{ 
 				  operation = line.toLowerCase();
 				  data = null;
 			  }
 			  
-			  try{ //perform commands
-				  // debug System.out.println("command: "+operation+"\tData: "+data);
+			  try{ 
 				  performCommand(operation, data);
 			  }catch(IllegalArgumentException err){
 				  System.err.println(err.getMessage());
-				  continue; //don't crash!
+				  continue;
 			  }
 		  }		  
 		  
-		  //Close the file
 		  reader.close(); 
 		  fhand.close();
 	  }catch(ArrayIndexOutOfBoundsException err){
@@ -57,19 +49,8 @@ public class A3Driver
 	  }
   }
   
-  /*----------------------------------------------------------------------------*/
-  /*---------------------Shopping Cart Command Methods--------------------------*/
-  /*----------------------------------------------------------------------------*/
-  
-  /** performCommand() **********************************************************
-   * Calls the correct command procedure based on user command string
-   * Forwards the data to each individual method (except print)
-   *               
-   * @param command : command string to be performed by program.
-   * @param data: data fields forwarded to respective command method.				
-   * ***************************************************************************/
+
   private static void performCommand(String command, String data){
-	  // resolves command
 	  command = command.trim();
 	  if(command.equals("insert")){
 		  insert(data);
@@ -93,25 +74,13 @@ public class A3Driver
 	  }
   }
   
-  /** Insert() ******************************************************************
-   * insert <category> <name> <price> <quantity> <weight> 
-   * 			<optional field1> <optional field2>
-   * 
-   * instantiates and object of the appropriate type (Groceries, Clothing, Electronics)
-   * and adds it to the shopping cart
-   * 
-   * @param data : the data string which contains the item fields
-   * ***************************************************************************/  
   private static void insert(String data){ 
 	 try{
-		 // create new cart item
 		 Item cart_item = createCartItem(data);
 		 String name = cart_item.getName();
 		 
-		 // insert item into cart
 		 addCartItem(cart_item);
 		 
-		 // successful insert message
 		 System.out.println(name + " was inserted into the cart.");
 		 
 	 }catch(NumberFormatException err){
@@ -124,24 +93,18 @@ public class A3Driver
 
   }
   
-  /** Search() ******************************************************************
-   * search <name> searches for all OBJECTS with name field as <name> and then
-   * outputs the number of OBJECTS found to the screen. 
-   * 
-   * @param data : the data string which contains the search name					
-   * ***************************************************************************/
   private static void search(String data){
-	  // extract search_name
+  	// get name
 	  String name;
 	  int npos = data.indexOf(' ');
 	  if(npos > 0){ 
 		  name = data.substring(0, npos).toLowerCase();
 	  }
-	  else{ //single parameter
+	  else{ 
 		  name = data;
 	  }
 	  
-	  //use iterators! (Or Binary Search ... your choice. nothing wrong w. a little O(n))
+
 	  int object_count = 0;
 	  int object_quantity = 0;
 	  Iterator<Item> cart_itr = shoppingCart.iterator();
@@ -159,14 +122,8 @@ public class A3Driver
 	  System.out.println("quantity of " + name + " objects: " + object_quantity);
   }
   
-  /** Delete() ******************************************************************
-   * delete <name> searches for and deletes
-   * all OBJECTS (not quantity) with the name field that matches the given <name>.
-   * 
-   * @param data : the data string which contains the search name	to delete				
-   * ***************************************************************************/
   private static void delete(String data){
-	  // extract search_name
+	// get name
 	  String name;	
 	  int npos = data.indexOf(' ');
 	  if(npos > 0){ 
@@ -176,7 +133,6 @@ public class A3Driver
 		  name = data;
 	  }
 	  
-	  //use iterators! (Or Binary Search ... your choice. nothing wrong w. a little O(n))
 	  int object_count = 0;
 	  Iterator<Item> cart_itr = shoppingCart.iterator();
 	  while(cart_itr.hasNext()){
@@ -192,27 +148,17 @@ public class A3Driver
 	  System.out.println(name + " objects deleted: " + object_count);
   }
   
-  /** Update() ******************************************************************
-   * update <name> <quantity> updates the quantity field for
-   * the first occurrence of a matching name.
-   * then output the name and new quantity value for that object to the screen.
-   * 
-   * @param data : the data string which contains the search name	to delete			
-   * ***************************************************************************/
   private static void update(String data){
 	  try{
-		  // parse update fields
 		  String[] fields = data.trim().split(" +");
 		  boolean item_found = false;
 		  String name = fields[0]; 
 		  int quantity = Integer.parseInt(fields[1]);
 		  
-		  // check valid quantity
 		  if(quantity < 0){
 			  throw new NumberFormatException();
 		  }
 		  
-		  //search for object to update
 		  Iterator<Item> cart_itr = shoppingCart.iterator();
 		  while(cart_itr.hasNext()){
 			  Item temp = cart_itr.next();
@@ -223,7 +169,7 @@ public class A3Driver
 					  cart_itr.remove();
 					  return;
 				  }
-				  else{ // item found, update quantity
+				  else{
 					  item_found = true;
 					  temp.setQuantity(quantity);
 					  break;
@@ -231,7 +177,6 @@ public class A3Driver
 			  }
 		  }
 		  
-		  // update results
 		  if(item_found){
 			  System.out.println(name + " quantity updated to: " + quantity);
 		  }
@@ -253,33 +198,22 @@ public class A3Driver
 
   }
   
-  /** Print() ******************************************************************
-   *  prints the contents of the shopping cart in ascending order by name.
-   *  prints each item's the name, quantity, price after tax and shipping charges
-   *  After, prints the total charges for entire cart.
-   *              				
-   * ***************************************************************************/
   private static void print(){
-	  // sorts the shopping Cart by name
-	  // (using merge sort? O(nlogn)?)
 	  Collections.sort(shoppingCart);
 	  
-	  // receipt output prompt
 	  System.out.print("\n");
 	  System.out.println("Printing Shopping Cart Receipt ... ");
 	  System.out.print("\n");
 	  
-	  // print cart items / calculate total
 	  float total_price = 0;
 	  Iterator<Item> cart_itr = shoppingCart.iterator();
 	  while(cart_itr.hasNext()){
 		  Item temp = cart_itr.next();
-		  System.out.print("\t"); //indent item fields
+		  System.out.print("\t");
 		  temp.printItemAttributes();
-		  total_price += temp.calculatePrice(); // update total
+		  total_price += temp.calculatePrice();
 	  }
 	  
-	  // total / thank the customer
 	  System.out.print("\n");
 	  System.out.println("Total: " + total_price);
 	  System.out.println("Thank you for shopping at 422CMart. Come again.");
@@ -287,17 +221,6 @@ public class A3Driver
 	  
   }
 
-  /*----------------------------------------------------------------------------*/
-  /*------------------------Shopping Cart Item Methods--------------------------*/
-  /*----------------------------------------------------------------------------*/
-  
-  /** addCartItem() *************************************************************
-   *  Checks to see if the item already exists in the cart.
-   *  If it does, it updates the existing quantity
-   *  Otherwise, it adds the item as a new addition
-   *              				
-   * @param obj : item to add to the cart
-   * ***************************************************************************/
   private static void addCartItem(Item obj){
 	  int obj_quantity = obj.getQuantity(); //quantity of new item
 	  Iterator<Item> cart_itr = shoppingCart.iterator();
@@ -313,19 +236,10 @@ public class A3Driver
 	  shoppingCart.add(obj);
   }
   
-  /** createCartItem() *********************************************************
-   *  Generates a new object from a data string
-   *  the data string contains the relevant object parameters
-   *              				
-   * @param obj_string : object fields for item to be created
-   * @return item : reference to the newly created cart item
-   * ***************************************************************************/
   private static Item createCartItem(String obj_string){
-	  // parse item fields
 	  String[] fields = obj_string.trim().split(" +");
 	  int fields_count = fields.length;
 	  
-	  // object data
 	  String life = ""; 
 	  String destination = ""; 
 	  String category = fields[0].toLowerCase(); 
@@ -334,7 +248,6 @@ public class A3Driver
 	  int quantity = Integer.parseInt(fields[3]);
 	  int weight = Integer.parseInt(fields[4]);
 		  
-	  // valid numerical input checks
 	  if(price < 0){
 		  throw new IllegalArgumentException(name + " cannot have a negative price!");
 	  }
@@ -345,7 +258,6 @@ public class A3Driver
 		  throw new IllegalArgumentException(name + " cannot have a negative weight");
 	  }
  
-	  // handle item creation by type
 	  if(category.equals("groceries")){
 		  Grocery grocery = new Grocery(name, price, quantity, weight);
 		  if(fields_count > 5){
@@ -374,12 +286,13 @@ public class A3Driver
 		  if(fields_count > 6){
 			  life = fields[5].toUpperCase();
 			  destination = fields[6].toUpperCase();
-			  if(life.equals("F")){ // fragile electronics
+			  // check for fragile
+			  if(life.equals("F")){
 				  electronic.setFragile(true);
 			  }
 			  electronic.setStateTax(destination);
 		  }
-		  // confirm valid destination state
+
 		  if(!Electronics.validState(destination)){
 			  throw new IllegalArgumentException(name + " cannot ship to " + destination);
 		  }
@@ -389,7 +302,7 @@ public class A3Driver
 		  Clothing clothing = new Clothing(name, price, quantity, weight);
 		  return clothing;
 	  }
-	  else{ // invalid category
+	  else{
 		  String errmsg = "Error: " + category + " is an invalid item category!\n";
 		  errmsg += "valid categories: groceries electronics clothing";
 		  throw new IllegalArgumentException(errmsg);
